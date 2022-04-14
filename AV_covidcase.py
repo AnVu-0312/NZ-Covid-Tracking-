9e39d7cfbbff1aea96b14265363e06de1d46e7b4
An Vu <vungocan0312@gmail.com>
2022-04-14 17:39:38 +1200

    Crawl body data but not table data

--- AV_covidcase.py
+++ AV_covidcase.py
@@ -8,9 +8,13 @@ class Covid_case(scrapy.Spider):
         url = 'https://www.health.govt.nz/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-current-cases'
         yield scrapy.Request (url = url, callback = self.parse)
     def parse (self, response):
-        table_data = response.css('div.table-responsive::text')
-        print(table_data)
+        html_file = 'web_body.html'
+        with open( html_file, 'wb' ) as fout:            
+            fout.write( response.body )
+  
+
 
 process = CrawlerProcess()
 process.crawl(Covid_case)
 process.start()
+print(dc_dict) 
