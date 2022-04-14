2417c8195d0d30fa6544a81d2fc131cdde4df0b3
An Vu <vungocan0312@gmail.com>
2022-04-14 22:21:23 +1200

    Update covid case crawling

--- AV_covidcase.py
+++ AV_covidcase.py
@@ -5,16 +5,18 @@ class Covid_case(scrapy.Spider):
     name = "Covid_case_crawling"
 
     def start_requests(self):
-        url = 'https://www.health.govt.nz/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-current-cases'
-        yield scrapy.Request (url = url, callback = self.parse)
+        urls = ["https://www.health.govt.nz/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-current-cases/"]
+        for url in urls:
+            yield scrapy.Request (url = url, callback = self.parse)
     def parse (self, response):
-        html_file = 'web_body.html'
-        with open( html_file, 'wb' ) as fout:            
-            fout.write( response.body )
-  
-
-
+        for row in response.xpath('//*[@class="table-style-two table"]//tbody/tr'):
+            yield {
+                'first' : row.xpath('td[1]//text()').extract_first(),
+                'last': row.xpath('td[2]//text()').extract_first(),
+                'handle' : row.xpath('td[3]//text()').extract_first(),
+            }
+            print(row)   
 process = CrawlerProcess()
 process.crawl(Covid_case)
 process.start()
-print(dc_dict) 
+
