7615b7c19019cc365366f2734841004539f98340
Binh <hoangthanh_binh8@yahoo.com>
2022-04-18 12:13:31 +1200

    sua phan loi khong chay

--- AV_covidcase.py
+++ AV_covidcase.py
@@ -1,3 +1,4 @@
+import json
 import scrapy 
 from scrapy.crawler import CrawlerProcess
 
@@ -9,13 +10,36 @@ class Covid_case(scrapy.Spider):
         for url in urls:
             yield scrapy.Request (url = url, callback = self.parse)
     def parse (self, response):
-        for row in response.xpath('//*[@class="table-style-two table"]//tbody/tr'):
+        print("\n\n\n\n\n\n\n\n\n\n")
+        print(response.css('.table-style-two>tbody>tr').extract_first())
+        print("\n\n\n\n\n\n\n\n\n\n")
+        # self.log(response)
+
+        # for row in response.xpath('//*[@class="table-style-two table"]//tbody/tr'):
+        for row in response.css('.table-style-two>tbody>tr'):
             yield {
                 'first' : row.xpath('td[1]//text()').extract_first(),
                 'last': row.xpath('td[2]//text()').extract_first(),
                 'handle' : row.xpath('td[3]//text()').extract_first(),
             }
-            print(row)   
+            # print(row)  
+            test = {
+                'first' : row.xpath('td[1]//text()').extract_first(),
+                'last': row.xpath('td[2]//text()').extract_first(),
+                'handle' : row.xpath('td[3]//text()').extract_first(),
+            }
+
+            # test = row.xpath('td[1]//text()').extract_first()
+            # test = {}
+            # test['first'] = row.xpath('td[1]//text()').extract_first()
+            # test['second'] = row.xpath('td[2]//text()').extract_first()
+            print("***************\n\n\n")
+            # print(row.xpath('td[1]//text()').extract_first())
+            # print(json.dump(test))
+            print(test)
+            print(json.dumps(test))
+            print("\n\n\n-----------------\n\n\n")
+
 process = CrawlerProcess()
 process.crawl(Covid_case)
 process.start()
